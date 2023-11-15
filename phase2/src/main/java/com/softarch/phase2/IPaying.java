eebca6c93c6f8ef555cbed19e52e2b02139bb319
Amr Mohamed Hassan <119889728+amrmoh-02@users.noreply.github.com>
2023-11-15 19:41:30 +0200

    Delete phase2/src/main/java/com/softarch/phase2 directory

--- phase2/src/main/java/com/softarch/phase2/IPaying.java
+++
@@ -1,6 +0,0 @@
-package com.softarch.phase2;
-
-public interface IPaying
-{ // this interface manages the different ways of payment
-    public boolean payment(Users l, String CardNo, String AmountToPay);
-}
